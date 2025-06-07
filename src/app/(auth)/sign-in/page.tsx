9e27b9ac12fdcb842e877bfcb7b4f5001e60a06b
Wisdom Mpamugo <wisdommpam99@gmail.com>
2025-06-07 22:48:09 +0100

    Auth Views

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import { SignInView } from "@/modules/auth/ui/views/sign-in-view"
+
+
+const SignInPage = () => {
+  return <SignInView />
+}
+
+export default SignInPage
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view"
+
+
+const SignUpPage = () => {
+  return <SignUpView />
+}
+
+export default SignUpPage
